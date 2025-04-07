da757fc3ad04d30d9bc7ec3782fb8948ac75fcfc
tcdw <tcdw2011@gmail.com>
2025-04-07 21:18:20 +0800

    make configuration notice clear

--- astro.config.js
+++ astro.config.js
@@ -9,7 +9,10 @@ export default defineConfig({
   // 请修改为你自己的线上地址，谢谢茄子
   site: 'https://tcdw.github.io/koi',
 
+  // 如果你的网站在子路径下（例如 `https://example.com/koi/`），则填写 `/koi/`
+  // 在根路径下（例如 `https://example.com/`）则填写 `/`
   base: process.env.NODE_ENV === "production" ? "/koi/" : "",
+
   integrations: [mdx(), sitemap(), svelte()],
 
   markdown: {
