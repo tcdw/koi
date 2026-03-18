5a17b1a493c5999df56f6fc5a8185862ac7a4419
tcdw <tcdw2011@gmail.com>
2026-03-18 20:33:19 +0800

    chore: upgrade to Astro 6

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,21 @@
+import type { APIContext } from "astro";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import { filterPosts } from "@/utils/misc";
+
+export async function GET(context: APIContext) {
+    const posts = filterPosts(await getCollection("blog"), {
+        filterDraft: true,
+        filterUnlisted: true,
+    });
+    return rss({
+        title: SITE_TITLE,
+        description: SITE_DESCRIPTION,
+        site: context.site!,
+        items: posts.map(post => ({
+            ...post.data,
+            link: `/blog/${post.id}/`,
+        })),
+    });
+}
